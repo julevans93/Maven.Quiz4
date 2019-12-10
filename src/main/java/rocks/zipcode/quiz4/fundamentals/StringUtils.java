908b5b7cff029badc45451267468f01abbce8d8b
Julia Evans <julevans93@users.noreply.github.com>
2019-12-10 07:29:16 -0500

    in progress

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -39,7 +39,7 @@ public class StringUtils {
         String s = str.toLowerCase();
         char[] array = s.toCharArray();
         Arrays.sort(array);
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length-1; i++){
             if (array[i] == array[i+1]){
                 return false;
             }
