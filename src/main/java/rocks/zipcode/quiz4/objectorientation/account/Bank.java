315174709b75ccc58e3f3753a249c56950aed3a7
Julia Evans <julevans93@users.noreply.github.com>
2019-12-09 22:24:25 -0500

    still working

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,35 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
+    List<BankAccount> bankAccounts;
+
     public BankAccount removeBankAccountByIndex(Integer indexNumber) {
+        for (int i = 0; i < bankAccounts.size(); i++){
+            if (i == indexNumber){
+                bankAccounts.remove(i);
+
+            }
+        }
         return null;
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        bankAccounts.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        //throw new UnsupportedOperationException("Method not yet implemented");
+        for (int i = 0; i < bankAccounts.size(); i++){
+            if (bankAccounts.get(i) == bankAccount){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -3,7 +3,31 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 27/12/2018.
  */
-public class BankAccount {
+public class BankAccount extends Account implements Transactable{
+    private Double val;
+
     public void setBalance(Double val) {
+        this.val = val;
+    }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) {
+       if (amountToIncreaseBy <= 0){
+           throw new IllegalArgumentException();
+       }
+        val += amountToIncreaseBy;
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        if (amountToDecreaseBy <= 0 || amountToDecreaseBy > val){
+            throw new IllegalArgumentException();
+        }
+        val -= amountToDecreaseBy;
+    }
+
+    @Override
+    public Double getBalance() {
+        return this.val;
     }
 }
